e6a9cbc3a0bdf721f89e957bd9cab75712abb43e
Caio <nascimento.csv@gmail.com>
2024-02-02 22:14:43 -0300

    Projeto de criação de VPC através do terraform

---
+++ AWS-VPC-TERRAFORM/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.0.0"
+    }
+  }
+    #backend é usado para configurar onde o estado remoto do terraform será armazenado
+  backend "s3" {
+    bucket = "bucketglobal-csv-nascimento"
+    key    = "aws-vpc/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+
+  default_tags {
+    tags = {
+      owner      = "caio"
+      managed-by = "terraform"
+    }
+  }
+}
