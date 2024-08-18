c543e9c83186251b8591fa5b9f97ade340d34bc6
Caio <nascimento.csv@gmail.com>
2024-08-17 21:47:09 -0300

    feat: fix locals

--- complex-project/aws-vpc-module/_locals.tf
+++ complex-project/aws-vpc-module/_locals.tf
@@ -7,14 +7,10 @@ locals {
   # AWS GENERAL
   # ==================================================================
   aws_default_tags = {
-    cost_allocation_business_unit  = "Caio"
-    cost_allocation_product        = "Caio"
-    operation_support_account_name = "${local.client_name}-${local.aws_environment}-services"
-    operation_support_criticality  = "high"
     operation_support_environment  = local.aws_environment
     operation_support_team         = "cloud"
-    source_code                    = "https://gitlab.com/nuageit/delivery/bhairport/cloud-management"
-    source_project                 = "setup-network"
+    source_code                    = "--"
+    source_project                 = "setup-vpc"
   }
   aws_environment = "Terraform"
   aws_region      = "us-east-1"
